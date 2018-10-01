a16b7af1edab642d239d8454714ac2d26be47d42
Chris Murray <chris.murray@s1now.com>
2018-10-01 09:14:50 +0100

    Better format HTML message and correct daemon name

--- cex_stock_checker.py
+++ cex_stock_checker.py
@@ -1,38 +1,31 @@
 #!python3
 
-import sys
-from configparser import ConfigParser
-import argparse
 import os
-from time import sleep
-from email.mime.multipart import MIMEMultipart
-from email.mime.text import MIMEText
+import argparse
+from configparser import ConfigParser
 import smtplib
 import logging
 import pickle
+from email.mime.multipart import MIMEMultipart
+from email.mime.text import MIMEText
 
 import requests
 
 CHECK_URL = "https://wss2.cex.uk.webuy.io/v3/boxes/{}/detail"
 
-EMAIL_HTML = r"""<html>
-    <head></head>
+EMAIL_HTML = r"""
+<html>
     <body>
-    <h1 style="font-size: 40px">CEX stock check</h1>
+    <h1>CEX stock check</h1>
     <p>
         <span style="font-size: 25px">In Stock:</span>
         <ul style="list-style-type:square">
-            <span style="font-size: 20px">{}</span>
+            <span style="font-size: 20px">{0}</span>
         </ul>
         <span style="font-size: 25px">Out of Stock:</span>
         <ul style="list-style-type:square">
-            <span style="font-size: 20px">{}</span>
+            <span style="font-size: 20px">{1}</span>
         </ul><br>
-
-        <span style="font-size: 25px">Items currently in the checklist:</span>
-        <ul style="list-style-type:square">
-            <span style="font-size: 20px">{}</span>
-        </ul>
     </p>
     </body>
 </html>"""
@@ -45,37 +38,41 @@ def check_stock():
         item_in_stock, item_name = check_item_in_stock(item)
         if item_in_stock:
             logger.info('Item %s in stock', item_name)
-            in_stock.append(item)
+            in_stock.append((item, item_name))
         else:
             logger.info('Item %s not in stock', item_name)
-            out_of_stock.append(item)
+            out_of_stock.append((item, item_name))
 
     if config['general']['persist'] or args.persist:
-        if os.path.isfile('items.dat'):
-            with open('items.dat', 'rb+') as persist_file:
-                persist_data = persist_file.read()
-                if persist_data:
-                    persist_dict = pickle.loads(persist_data)
-                    for item in in_stock:
-                        if not persist_dict.get(item):
-                            email_trigger = True
-                elif in_stock:
-                    email_trigger = True
-                persist_dict = {item_name:True for item_name in in_stock}
-                persist_dict.update({item_name:False for item_name in out_of_stock})
-                persist_file.write(pickle.dumps(persist_dict))
-        else:
-            with open('items.dat', 'wb+') as persist_file:
-                persist_dict = {item_name:True for item_name in in_stock}
-                persist_dict.update({item_name:False for item_name in out_of_stock})
-                persist_file.write(pickle.dumps(persist_dict))
-            if in_stock:
-                email_trigger = True
+        email_trigger = check_persist(in_stock, out_of_stock)
     else:
         email_trigger = True
     if config['general']['send_email_enabled'] and email_trigger:
         send_email(in_stock, out_of_stock)
 
+
+def check_persist(in_stock, out_of_stock):
+    if os.path.isfile('items.dat'):
+        with open('items.dat', 'rb+') as persist_file:
+            persist_data = persist_file.read()
+            if persist_data:
+                persist_dict = pickle.loads(persist_data)
+                for item in in_stock:
+                    if not persist_dict.get(item):
+                        return True
+            elif in_stock:
+                return True
+            persist_dict = {item_name:True for item_name in in_stock}
+            persist_dict.update({item_name:False for item_name in out_of_stock})
+            persist_file.write(pickle.dumps(persist_dict))
+    else:
+        with open('items.dat', 'wb+') as persist_file:
+            persist_dict = {item_name:True for item_name in in_stock}
+            persist_dict.update({item_name:False for item_name in out_of_stock})
+            persist_file.write(pickle.dumps(persist_dict))
+        if in_stock:
+            return True
+    return False
 
 def check_item_in_stock(item):
     response = requests.get(CHECK_URL.format(item)).json()
@@ -90,14 +87,22 @@ def send_email(in_stock, out_of_stock):
     msg['From'] = config['general']['email_send_from']
     msg['to'] = config['general']['to_email']
 
+    html_in_stock = ''
+    for item in in_stock:
+        _, item_name = item
+        html_in_stock = html_in_stock + '<li>{0}</li>'.format(item_name)
+    html_out_of_stock = ''
+    for item in out_of_stock:
+        _, item_name = item
+        html_out_of_stock = html_out_of_stock + '<li>{0}</li>'.format(item_name)
 
     html = MIMEText(
         str.format(
             EMAIL_HTML,
-            in_stock,
-            out_of_stock,
-            config['general']['items'].split(',')
-        )
+            html_in_stock,
+            html_out_of_stock
+        ),
+        'html'
     )
     msg.attach(html)
 
@@ -113,8 +118,8 @@ def send_email(in_stock, out_of_stock):
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(
         description="Check cex stock for all items listed in the config.")
-    parser.add_argument('--deamon', action='store_true',
-                        help='In deamon mode, nothing will be output to screen')
+    parser.add_argument('--daemon', action='store_true',
+                        help='In daemon mode, nothing will be output to screen')
     parser.add_argument('--config-path', help='Path to the config file, default to config.ini in the same folder as the script.', default='config.ini')
     parser.add_argument('--persist', help='Persist stock changes to a file, and only trigger an email if the stock has changed.', action='store_true')
 
@@ -128,8 +133,8 @@ if __name__ == '__main__':
 
     stdouthandler = logging.StreamHandler()
     stdouthandler.setFormatter(formatter)
-    stdouthandler.setFormatter(formatter)
-    logger.addHandler(stdouthandler)
+    if not args.daemon:
+        logger.addHandler(stdouthandler)
 
     if os.path.isfile(args.config_path):
         config = ConfigParser()
